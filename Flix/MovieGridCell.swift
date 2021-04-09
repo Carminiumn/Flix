5b824e40774796bfba6a171f0cc2214f8ece96e0
Carminiumn <carminiumn@gmail.com>
2021-04-09 12:45:27 -0400

    part 2 update

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by KagurazakaX on 4/7/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
